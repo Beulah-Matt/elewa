83fbed310586bde6a6766b498c83505f0bff4f6b
Reagan Charana <rgncharana2@gmail.com>
2024-10-29 13:00:54 +0300

    :bug: prevent multiple state instances

--- libs/features/convs-mgr/stories/builder/flow-builder/state/src/flow-builder.module.ts
+++ libs/features/convs-mgr/stories/builder/flow-builder/state/src/flow-builder.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MultiLangModule } from '@ngfi/multi-lang';
 
-import { FlowBuilderStateProvider } from './lib/state/flow-builder-state.provider';
+// import { FlowBuilderStateProvider } from './lib/state/flow-builder-state.provider';
 
 @NgModule({
   imports: [
     CommonModule, MultiLangModule
   ],
-  providers: [
-    FlowBuilderStateProvider
-  ],
+  providers: [ ],
 })
 export class FlowBuilderStateModule { }
