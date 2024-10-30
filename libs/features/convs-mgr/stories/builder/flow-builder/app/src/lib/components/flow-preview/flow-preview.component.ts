48e5c4e240d1f50640c63d63f323a4daf9e18ab2
Reagan Charana <rgncharana2@gmail.com>
2024-10-30 18:34:31 +0300

    :lipstick: fetch preview url from backend

--- libs/features/convs-mgr/stories/builder/flow-builder/app/src/lib/components/flow-preview/flow-preview.component.ts
+++ libs/features/convs-mgr/stories/builder/flow-builder/app/src/lib/components/flow-preview/flow-preview.component.ts
@@ -1,6 +1,10 @@
 import { SubSink } from 'subsink';
 
+import { filter, switchMap } from 'rxjs';
+
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { WFlowService } from '@app/state/convs-mgr/wflows';
+import { FlowBuilderStateProvider } from '@app/features/convs-mgr/stories/builder/flow-builder/state';
 
 @Component({
   selector: 'app-flow-preview',
@@ -10,14 +14,33 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 export class FlowPreviewComponent implements OnInit, OnDestroy 
 {
   private _sbS = new SubSink();
+  preview: {url: string, expiry: Date};
 
-  constructor() 
+  constructor(private _wFlowService: WFlowService, private _flowStateService: FlowBuilderStateProvider) 
   { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    const state$ = this._flowStateService.state$;
+    this._sbS.sink = state$.pipe(filter((state)=> !!state), switchMap((state)=> {
+      const flowId = state.flow.flow.id;
+
+      return this._wFlowService.getPreview(flowId);
+    })).subscribe((data)=> {
+      if(data.error) {
+        // TODO: Toast error message
+        console.log(data.error);  
+      } else {
+        this.preview = {
+          url: data.preview.preview_url,
+          expiry: data.preview.expires_at
+        }
+
+      }
+    })
+   }
 
 
   ngOnDestroy(): void {
-      
+      this._sbS.unsubscribe();
   }
 }
