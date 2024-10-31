99f5bd27e6cd26bc78f8f9f7b8934fa07f9c644b
Fraciah Karagu <karfraciah@gmail.com>
2024-10-31 13:25:30 +0300

    Refactor flow component form creation logic

--- libs/features/convs-mgr/stories/builder/flow-builder/app/src/lib/providers/flow-forms-build-factory.util.ts
+++ libs/features/convs-mgr/stories/builder/flow-builder/app/src/lib/providers/flow-forms-build-factory.util.ts
@@ -1,31 +1,30 @@
 import { FormBuilder } from "@angular/forms";
 
-import { FlowPageLayoutElementTypesV31, FlowPageLayoutElementV31 } from "@app/model/convs-mgr/stories/flows";
+import { FlowControl, FlowControlType, FlowPageLayoutElementTypesV31, FlowPageLayoutElementV31, FlowTextAreaInput, FlowTextInput } from "@app/model/convs-mgr/stories/flows";
 
 import { _CreateFlowTextForm } from "./flow-forms/flow-text-form-build.model";
 import { _CreateFlowTextAreaInputForm } from './flow-forms/flow-text-area-form-build.model'
 import { _CreateFlowDatePickerInputForm} from './flow-forms/flow-date-input-build.model'
-// import { _CreateFlowTextInputForm } from './flow-forms/flow-text-input-form-build.model'
+import { _CreateFlowTextInputForm } from "./flow-forms/flow-text-input-form-build.model";
 
-export function _GetFlowComponentForm(_fb: FormBuilder, componentData?: FlowPageLayoutElementV31)
-{
+export function _GetFlowComponentForm(_fb: FormBuilder, componentData?: FlowPageLayoutElementV31) {
+  const componentDataType = componentData as FlowControl;
+  if (componentDataType ) {
+    switch (componentDataType.controlType) {
+      case FlowControlType.Text:
+        return _CreateFlowTextForm(_fb, componentData);
+
+      case FlowControlType.TextArea:
+        return _CreateFlowTextAreaInputForm(_fb, componentData as FlowTextAreaInput);
+
+      // case FlowPageLayoutElementTypesV31.DATE_PICKER_INPUT:
+      //   return _CreateFlowDatePickerInputForm(_fb, componentData);
+
+      case FlowControlType.TextInput:
+        return _CreateFlowTextInputForm(_fb, componentData as FlowTextInput);
+
+    }
+  }
+  // Default return null
   return  _CreateFlowTextForm(_fb, componentData);
-  // if(componentData){
-  //   switch (componentData.type) 
-  // {
-  //   case FlowPageLayoutElementTypesV31.TEXT:
-  //     return  _CreateFlowTextForm(_fb, componentData)
-
-  //   // case FlowPageLayoutElementTypesV31.TEXT_AREA_INPUT:
-  //   //   return  _CreateFlowTextAreaInputForm(_fb, componentData)
-
-  //   // case FlowPageLayoutElementTypesV31.DATE_PICKER_INPUT:
-  //   //   return  _CreateFlowDatePickerInputForm(_fb, componentData)
-
-  //   // case FlowPageLayoutElementTypesV31.TEXT_INPUT:
-  //   //   return  _CreateFlowTextInputForm(_fb, componentData)
-  // }
-  // }
-  // // Default return null
-  // return undefined;
 }
--- libs/features/convs-mgr/stories/builder/flow-builder/app/src/lib/providers/flow-forms/flow-text-input-form-build.model.ts
+++ libs/features/convs-mgr/stories/builder/flow-builder/app/src/lib/providers/flow-forms/flow-text-input-form-build.model.ts
@@ -1,13 +1,14 @@
-// import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-// import { FlowPageLayoutElementV31} from "@app/model/convs-mgr/stories/flows";
-// import {FlowPageLayoutElementTypesV31} from "@app/model/convs-mgr/stories/flows";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { FlowPageLayoutElementV31, FlowTextInput} from "@app/model/convs-mgr/stories/flows";
+import {FlowPageLayoutElementTypesV31} from "@app/model/convs-mgr/stories/flows";
 
-// /**
-//  * 
-//  * @param _fb instance of formbuilder that creates formgroups controls etc
-//  * @param blockData the data being patched into the FormGroup
-//  * @returns builds the formgroup with data if available and returns the Formgroup
-//  */
+/**
+ * 
+ * @param _fb instance of formbuilder that creates formgroups controls etc
+ * @param blockData the data being patched into the FormGroup
+ * @returns builds the formgroup with data if available and returns the Formgroup
+ */
+
 // export function _CreateFlowTextInputForm(_fb: FormBuilder, blockData?: FlowPageLayoutElementV31 ): FormGroup 
 // {
 //   return _fb.group({
@@ -22,3 +23,18 @@
 //       visible: [blockData?.visible ?? '']
 //   });
 // }
+
+export function _CreateFlowTextInputForm(_fb: FormBuilder, blockData?: FlowTextInput ): FormGroup 
+{
+  return _fb.group({
+    name: [blockData?.name ?? "", Validators.required],
+      label: [blockData?.label ?? "", Validators.required],
+      required: [blockData?.required ?? "", Validators.required],
+      type: [FlowPageLayoutElementTypesV31.TEXT_INPUT],
+      inputType: [blockData?.["input-type"] ?? ''],
+      minChars: [blockData?.["min-chars"]?? null, Validators.min(1)],
+      maxChars: [blockData?.["max-chars"]?? null, Validators.max(255)],
+      helperText: [blockData?.["helper-text"] ?? null, Validators.maxLength(80)],
+      visible: [blockData?.visible ?? '']
+  });
+}
