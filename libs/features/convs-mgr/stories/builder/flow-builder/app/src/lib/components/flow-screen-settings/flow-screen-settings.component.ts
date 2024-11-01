7029284f34da544cac8df58009cbbe1470e7ca50
Fraciah Karagu <karfraciah@gmail.com>
2024-11-01 14:22:45 +0300

    ✨  Add screen setttings content

--- libs/features/convs-mgr/stories/builder/flow-builder/app/src/lib/components/flow-screen-settings/flow-screen-settings.component.ts
+++ libs/features/convs-mgr/stories/builder/flow-builder/app/src/lib/components/flow-screen-settings/flow-screen-settings.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'lib-flow-screen-settings',
   templateUrl: './flow-screen-settings.component.html',
   styleUrl: './flow-screen-settings.component.scss',
 })
-export class FlowScreenSettingsComponent {}
+export class FlowScreenSettingsComponent {
+  @Output() showScreenSettings = new EventEmitter<boolean>();
+
+  isChecked = false;
+
+  currentScreenChar = 0;
+  readonly screenMaxChar = 20;
+
+  onInputChange(event: KeyboardEvent){
+    const input = event.target as HTMLInputElement;
+    this.currentScreenChar = input.value.length;
+
+    if(this.currentScreenChar > this.screenMaxChar){
+      input.value = input.value.slice(0, this.screenMaxChar);
+    }
+  }
+
+  closeScreenSettings(){
+    this.showScreenSettings.emit(false);
+  }
+}
