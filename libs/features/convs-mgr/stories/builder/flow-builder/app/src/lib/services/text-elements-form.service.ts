1dc4a9897309731c292ba82f70f9aa8016ebe02f
Reagan Charana <rgncharana2@gmail.com>
2024-11-05 18:58:59 +0300

    :fire: remove redundant function

--- libs/features/convs-mgr/stories/builder/flow-builder/app/src/lib/services/text-elements-form.service.ts
+++ libs/features/convs-mgr/stories/builder/flow-builder/app/src/lib/services/text-elements-form.service.ts
@@ -23,8 +23,7 @@ export class TextElementFormService
   {
     return this._formBuilder.group({
       text: [textElement.text ?? ''],
-      type: [textElement.type ?? ''],
-      size: [textElement.size ?? '']
+      type: [textElement.type ?? '']
     })
   }
   
@@ -39,41 +38,6 @@ export class TextElementFormService
       type: ['', Validators.required],
       size: ['', Validators.required]
     })
-  }
-
-  /**
-   * Function to turn form values to a valid text element object
-   * @param textType Basic interface for use on F.E only, simplifies a flow control
-   * @returns FlowPageTextV31
-   */
-  transformElement(textType: EditableTextElement): FlowPageTextV31
-  {
-    let textSize = FlowPageTextSizesV31.Body; // Default size
-  
-    // Determine the size based on the type
-    switch (textType.size) {
-      case FlowControlType.Header:
-        textSize = FlowPageTextSizesV31.Header;
-        break;
-      case FlowControlType.LightHeader:
-        textSize = FlowPageTextSizesV31.SubHeader;
-        break;
-      case FlowControlType.Text:
-        textSize = FlowPageTextSizesV31.Body;
-        break;
-      case FlowControlType.Caption:
-        textSize = FlowPageTextSizesV31.Caption;
-        break;
-      default:
-        console.warn('Unrecognized control type, using default size.');
-    }
-  
-    // Return the transformed FlowPageTextV31 object
-    return {
-      text: textType.text, 
-      type: FlowPageLayoutElementTypesV31.TEXT,
-      size: textSize               
-    };
   }
 }
 
